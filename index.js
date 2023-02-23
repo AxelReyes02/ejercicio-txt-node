3a1aa8412ba17d2f65854528eed9f584560a6175
AxelReyes02 <119076418+AxelReyes02@users.noreply.github.com>
2023-02-22 19:01:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs-extra');
+
+
+// Traemos el link desde un archivo txt
+const link = fs.readFileSync('./link.txt', 'utf-8');
+
+
+// Hace referencia a lo asincrono
+// Asincronos y sincronos ( al instante )
+// Un buffer es un archivo
+
+
+// Traer el html de la pagina
+
